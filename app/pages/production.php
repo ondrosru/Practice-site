296c8aaeea83edc4f703f3dcd6d52ea538f90fbb
Carapacik <carapaciK@gmail.com>
2020-08-28 23:05:08 +0300

    add sliders

---
+++ app/pages/production.php
@@ -0,0 +1,136 @@
+<header class="entry-header">
+	<h2 class="entry-title">Добро пожаловать в АВИДЖИ</h2>
+</header>
+	<h2>Электротехнические компоненты, промышленная пневматика и электро-механические системы для автоматизации производства</h2>
+<div>
+	<ul ><li>SIEMENS</li><li>FINDER</li><li>PHOENIX CONTACT</li><li>EATON</li><li>MOELLER</li><li>DELTA</li><li>LEUZE</li><li>FESTO</li><li>OMRON</li><li>SICK</li><li>IFM</li></ul>
+</div>
+<div id="carousel1" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel1" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel1" data-slide-to="1"></li>
+    <li data-target="#carousel1" data-slide-to="2"></li>
+    <li data-target="#carousel1" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./../public/images/2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/1-2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/8-3.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/ss.jpg">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only indicator-carousel-avg">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<h2 id="">Цепи роликовые и звездочки к ним</h2>
+<div>
+<ul id="block-3f0f8eef-b363-42ec-8159-e80b11b598a9"><li>DITTON (Латвия)</li><li>SKF (Германия)</li><li>IWIS (Германия)</li></ul></div>
+<div id="carousel2" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel2" data-slide-to="4" class="active"></li>
+    <li data-target="#carousel2" data-slide-to="5"></li>
+    <li data-target="#carousel2" data-slide-to="6"></li>
+    <li data-target="#carousel2" data-slide-to="7"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./../public/images/2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/1-2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/8-3.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/ss.jpg">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel2" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only indicator-carousel-avg">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel2" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+	<h3>Сетки отечественного производства</h3>
+<div><ul id="block-3f0f8eef-b363-42ec-8159-e80b11b598a9"><li>DITTON (Латвия)</li><li>SKF (Германия)</li><li>IWIS (Германия)</li></ul></div>
+<div id="carousel3" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel3" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel3" data-slide-to="1"></li>
+    <li data-target="#carousel3" data-slide-to="2"></li>
+    <li data-target="#carousel3" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./../public/images/2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/1-2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/8-3.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/ss.jpg">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel3" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only indicator-carousel-avg">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel3" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<h2>Вакуумные присоски</h2>
+<div>
+	<ul><li>FIPA</li></ul>
+</div>
+<div id="carousel4" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel4" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel4" data-slide-to="1"></li>
+    <li data-target="#carousel4" data-slide-to="2"></li>
+    <li data-target="#carousel4" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="./../public/images/2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/1-2.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/8-3.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="./../public/images/ss.jpg">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel4" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only indicator-carousel-avg">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel4" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php 
 include_once 'app/components/header.php';
+include_once 'app/pages/production.php';
 include_once 'app/components/footer.php';
