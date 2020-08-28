a3b6b54f4ec076d1874ef7a8082f85fd4a824e05
Артемий <chekanovartemii99@mail.ru>
2020-08-28 20:49:05 +0300

    навигация (#1)
    Добавлена навигация

---
+++ app/components/contacts.php
@@ -0,0 +1,26 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style/main.css">
+        <title>Main</title>
+        <script src="js/navigation.js"></script>
+    </head>
+    <body>
+        <div class="menu-primary-2-container"> 
+            <ul class="menu">
+                <li class="menu-item">
+                    <button class="" onclick="jumpMain()">Главная страница</button>
+                </li>
+                <li class="menu-item">
+                    <button class="" onclick="jumpInformation()">О нас</button>
+                </li>
+                <li class="menu-item">
+                    <button class="" onclick="jumpProducts()">Продукция</button>
+                </li>
+                <li class="menu-item">
+                    <button class="" onclick="jumpContact()">Контакты !!!</button>
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
