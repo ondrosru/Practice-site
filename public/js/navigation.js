514ddf43f344a7d22baf7805e5f3f14502d8f576
ondrosru <izickov@yandex.ru>
2020-08-28 21:08:41 +0300

    Заменил названия хоста на относительный путь

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,15 +1,15 @@
 function jumpMain() {
-    window.location.href = "http://localhost/app\components/main.php"; // адрес на Главнус страницу
+    window.location.href = "./../../app/components/main.php"; // адрес на Главнус страницу
 }
 
 function jumpInformation() {
-    window.location.href = "http://localhost/app\components/information.php"; // адрес на страницу О нас
+    window.location.href = "./../../app/components/information.php"; // адрес на страницу О нас
 }
 
 function jumpProducts() {
-    window.location.href = "http://localhost/app\components/products.php"; // адрес на страницу Продукция
+    window.location.href = "./../../app/components/products.php"; // адрес на страницу Продукция
 }
 
 function jumpContact() {
-    window.location.href = "http://localhost/app\components/contacts.php"; // адрес на страницу Контактов
+    window.location.href = "./../../app/components/contacts.php"; // адрес на страницу Контактов
 }
