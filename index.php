dadeb1deff84b5eb361fb96a32f8b12671c0af8a
ondrosru <izickov@yandex.ru>
2020-08-28 16:34:58 +0300

    Added bootstrap-4

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>ООО «Авиджи»</title>
+    <link rel="stylesheet" type="text/css" href="/modules/bootstrap/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="/public/images/logo.png" type="image/png">
+  </head>
+  <body>
+    <div class="container">
+        <h1>Привет мир1</h1>
+    </div>
+  </body>
+</html>
