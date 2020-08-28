6f63e577cf1e5fb7558bbce3adc3ff3fc4654ee4
Sancene <minefattoproject@gmail.com>
2020-08-29 02:38:29 +0300

    about page added

---
+++ app/page/about/about.php
@@ -0,0 +1,55 @@
+<header class="entry-header">
+    <h2 class="entry-title">Добро пожаловать в АВИДЖИ</h2>
+</header>
+<p class="has-text-align-center">
+    ООО <b>«АВИДЖИ»</b> — молодая, надежная, динамично развивающаяся компания
+    в сфере комплексных поставок оригинальных комплектующих и их аналогов для промышленного оборудования.
+</p>
+<div class="production-list-wrapper">
+    <p class="has-text-align-center">
+        Наши специалисты отлично знают номенклатуру различных производителей:
+    </p>
+    <div>
+        <ul ><li>SIEMENS</li><li>FINDER</li><li>DAFNOSS</li><li>GRUNDFOSS</li><li>PHOENIX CONTACT</li><li>EATON</li><li>MOELLER</li><li>DELTA</li><li>RITTAL</li></ul>
+    </div>
+    <div id="carousel1" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="../../../public/images/slider_1.png" alt="Image not found">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="../../../public/images/slider_2.png" alt="Image not found">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="../../../public/images/slider_3.jpg" alt="Image not found">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="../../../public/images/slider_4.jpg" alt="Image not found">
+            </div>
+        </div>
+        <ol class="carousel-indicators">
+            <li data-target="#carousel1" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel1" data-slide-to="1"></li>
+            <li data-target="#carousel1" data-slide-to="2"></li>
+            <li data-target="#carousel1" data-slide-to="3"></li>
+        </ol>
+        <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev">
+            <div class="button_style">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only indicator-carousel-avg">Previous</span>
+            </div>
+        </a>
+        <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next">
+            <div class="button_style">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </div>
+        </a>
+    </div>
+</div>
+<p class="has-text-align-center">
+    Наша компания готова предложить выгодные условия сотрудничества, ответить на любые Ваши вопросы. Благодаря грамотно организованной логистике, поставки осуществляются в предельно сжатые сроки.
+</p>
+<p class="has-text-align-center">
+    Все усилия наших сотрудников направлены на установление, расширение и дальнейшее укрепление долгосрочных партнерских отношений с нашими клиентами.
+</p>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
 include_once 'app/components/header.php';
+?>
+    <div class="container" style="max-width: 740px;">
+<?php
+include_once 'app/page/about/about.php';
+?>
+    </div>
+<?php
 include_once 'app/components/footer.php';
+?>
