2866090ddb1afe90148461584689923192f23cd2
Margo <mnchek@mail.ru>
2020-08-28 20:13:17 +0300

    навигация

---
+++ public/js/navigation.js
@@ -0,0 +1,15 @@
+function jumpMain() {
+    window.location.href = "http://localhost/wdb-course-3/software/notebook/input.php"; // адрес на Главнус страницу
+}
+
+function jumpInformation() {
+    window.location.href = "http://localhost/wdb-course-3/software/notebook/input.php"; // адрес на страницу О нас
+}
+
+function jumpProducts() {
+    window.location.href = "http://localhost/wdb-course-3/software/notebook/input.php"; // адрес на страницу Продукция
+}
+
+function jumpContact() {
+    window.location.href = "http://localhost/wdb-course-3/software/notebook/input.php"; // адрес на страницу Контактов
+}
